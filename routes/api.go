66dbabcb50ec456f897c06b5de57d9b4cf223aca
aaron <616915054@qq.com>
2022-03-06 11:54:08 +0800

    发送短信验证码

--- routes/api.go
+++ routes/api.go
@@ -22,6 +22,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
 			// 图片验证码，需要加限流
+			authGroup.POST("/verify-codes/phone",vcc.SendUsingPhone)
 			authGroup.POST("/verify-codes/captcha",vcc.ShowCaptcha)
 
 		}
