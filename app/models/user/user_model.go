07100f93ec41d2476a604a245e51781235bd7923
aaron <616915054@qq.com>
2022-03-13 17:07:05 +0800

    用户列表

--- app/models/user/user_model.go
+++ app/models/user/user_model.go
@@ -28,3 +28,9 @@ func (userModel *User) Create() {
 func (userModel *User) ComparePassword(_password string) bool {
 	return hash.BcryptCheck(_password, userModel.Password)
 }
+
+// All 获取所有用户数据
+func All() (users []User) {
+	database.DB.Find(&users)
+	return
+}
