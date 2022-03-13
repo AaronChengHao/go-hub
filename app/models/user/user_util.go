6817700bb59a82410572020be595f3759acc136b
aaron <616915054@qq.com>
2022-03-13 17:14:08 +0800

    用户列表分页

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -1,7 +1,10 @@
 package user
 
 import (
+	"github.com/gin-gonic/gin"
+	"gohub/pkg/app"
 	"gohub/pkg/database"
+	"gohub/pkg/paginator"
 )
 
 // IsEmailExist 判断 Email 已被注册
@@ -48,5 +51,17 @@ func (userModel *User) Save() (rowsAffected int64) {
 // GetByEmail 通过 Email 来获取用户
 func GetByEmail(email string) (userModel User) {
 	database.DB.Where("email = ?", email).First(&userModel)
+	return
+}
+
+// Paginate 分页内容
+func Paginate(c *gin.Context, perPage int) (users []User, paging paginator.Paging) {
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(User{}),
+		&users,
+		app.V1URL(database.TableName(&User{})),
+		perPage,
+	)
 	return
 }
